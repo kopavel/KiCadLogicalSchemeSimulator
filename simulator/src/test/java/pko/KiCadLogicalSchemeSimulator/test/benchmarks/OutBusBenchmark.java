6fb326fb5f8886d3d7c30b08790043961a6dae21
pavels.korzs <kopavel@inbox.lv>
2024-08-07 13:19:41 +0300

    benchmarking

---
+++ simulator/src/test/java/pko/KiCadLogicalSchemeSimulator/test/benchmarks/OutBusBenchmark.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2024 Pavel Korzh
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package pko.KiCadLogicalSchemeSimulator.test.benchmarks;
+import org.openjdk.jmh.annotations.*;
+import pko.KiCadLogicalSchemeSimulator.api.bus.Bus;
+import pko.KiCadLogicalSchemeSimulator.api.bus.OutBus;
+import pko.KiCadLogicalSchemeSimulator.api.schemaPart.SchemaPart;
+
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+public class OutBusBenchmark {
+    @Benchmark
+    @Fork(value = 1)
+    @Warmup(iterations = 1, time = 5)
+    @Measurement(iterations = 3, time = 5)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void iterator(StateForIterator state) {
+        Runner.doWork(state.out);
+    }
+
+    @Benchmark
+    @Fork(value = 1)
+    @Warmup(iterations = 1, time = 5)
+    @Measurement(iterations = 3, time = 5)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void javac(StateForJavac state) {
+        Runner.doWork(state.out);
+    }
+
+    @Benchmark
+    @Fork(value = 1)
+    @Warmup(iterations = 1, time = 5)
+    @Measurement(iterations = 3, time = 5)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void optimiser(StateForOptimiser state) {
+        Runner.doWork(state.out);
+    }
+
+    @State(Scope.Thread)
+    public static class StateForIterator {
+        Bus out;
+
+        @Setup(Level.Trial)
+        public void setUp() {
+            SchemaPart testPart = new SchemaPart("iterator", "") {
+                @Override
+                public void initOuts() {
+                }
+            };
+            out = new OutBus("test", testPart, 5);
+            for (int i = 0; i < 5; i++) {
+                ((OutBus) out).addDestination(testPart.addInBus("in" + i, 5), 0x31, (byte) 0);
+            }
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class StateForJavac {
+        Bus out;
+
+        @Setup(Level.Trial)
+        public void setUp() {
+            SchemaPart testPart = new SchemaPart("javac", "") {
+                @Override
+                public void initOuts() {
+                }
+            };
+            out = new OutBus("test", testPart, 5);
+            for (int i = 0; i < 5; i++) {
+                ((OutBus) out).addDestination(testPart.addInBus("in" + i, 5), 0x31, (byte) 0);
+            }
+            out = out.getOptimised();
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class StateForOptimiser {
+        Bus out;
+
+        @Setup(Level.Trial)
+        public void setUp() {
+            SchemaPart testPart = new SchemaPart("Optimiser", "") {
+                @Override
+                public void initOuts() {
+                }
+            };
+            out = new OutBus("test", testPart, 5);
+            for (int i = 0; i < 5; i++) {
+                ((OutBus) out).addDestination(testPart.addInBus("in" + i, 5), 0x31, (byte) 0);
+            }
+            out = out.getOptimised();
+        }
+    }
+}
--- simulator/src/test/java/pko/KiCadLogicalSchemeSimulator/test/benchmarks/OutPinBenchmark.java
+++ simulator/src/test/java/pko/KiCadLogicalSchemeSimulator/test/benchmarks/OutPinBenchmark.java
@@ -1,93 +1,69 @@
-package pko.KiCadLogicalSchemeSimulator.test;
+/*
+ * Copyright (c) 2024 Pavel Korzh
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package pko.KiCadLogicalSchemeSimulator.test.benchmarks;
 import org.openjdk.jmh.annotations.*;
 import pko.KiCadLogicalSchemeSimulator.api.schemaPart.SchemaPart;
 import pko.KiCadLogicalSchemeSimulator.api.wire.OutPin;
 import pko.KiCadLogicalSchemeSimulator.api.wire.Pin;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
 public class OutPinBenchmark {
-    public static void main(String[] args) throws IOException {
-        String[] benchmarks = {//
-//                "pko.KiCadLogicalSchemeSimulator.test.OutPinBenchmark.iterator",//
-//                "pko.KiCadLogicalSchemeSimulator.test.OutPinBenchmark.javac",//
-                "pko.KiCadLogicalSchemeSimulator.test.OutPinBenchmark.optimiser"//
-        };
-        org.openjdk.jmh.Main.main(benchmarks);
-    }
-
     @Benchmark
     @Fork(value = 1)
-    @Warmup(iterations = 1)
-    @Measurement(iterations = 3, time = 15)
+    @Warmup(iterations = 1, time = 5)
+    @Measurement(iterations = 3, time = 5)
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void iterator(StateForIterator state) {
-        doWork(state.out);
+        Runner.doWork(state.out);
     }
 
     @Benchmark
     @Fork(value = 1)
-    @Warmup(iterations = 1)
-    @Measurement(iterations = 3, time = 15)
+    @Warmup(iterations = 1, time = 5)
+    @Measurement(iterations = 3, time = 5)
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void javac(StateForJavac state) {
-        doWork(state.out);
+        Runner.doWork(state.out);
     }
 
     @Benchmark
     @Fork(value = 1)
-    @Warmup(iterations = 1)
-    @Measurement(iterations = 3, time = 15)
+    @Warmup(iterations = 1, time = 5)
+    @Measurement(iterations = 3, time = 5)
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void optimiser(StateForOptimiser state) {
-        doWork(state.out);
-    }
-
-    private static void doWork(Pin out) {
-        for (int i = 0; i < 100; i++) {
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-            out.state = true;
-            out.setState(true);
-            out.state = false;
-            out.setState(false);
-        }
+        Runner.doWork(state.out);
     }
 
     @State(Scope.Thread)
